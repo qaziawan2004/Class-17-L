04eee067e9d381c303809fff71871dd39e4a1e19
qaziawan2004 <qaziawan120@gmail.com>
2026-01-05 20:43:40 +0500

    task 2 updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ function App() {
     { id: 1, name: "Sami", rollNo: 1, batch: 16, favLanguage: "Python" },
     { id: 2, name: "Faisal", rollNo: 2, batch: "WMA 14", favLanguage: "C#" },
     { id: 3, name: "Wahab", rollNo: 3, batch: "WMA 13", favLanguage: "JavaScript" },
-    { id:  4, name: "Ahmed", rollNo: 4, batch: "WMA 12", favLanguage: "Java" },
+    { id: 4, name: "Ahmed", rollNo: 4, batch: "WMA 12", favLanguage: "Java" },
     { id: 5 , name: "Meraj", rollNo: 5, batch: "WMA 10", favLanguage: "Urdu" },
   ];
-  let setState = useState(0);
- let count = setState[0];
- let setCount = setState[1]; 
+const[count ,setCount] = useState(0)
 
-function decrease () {
-  setCount (count - 1)
-}
+ function decrease() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
 
 function increase () {
   setCount(count + 1)
@@ -39,12 +39,18 @@ function reSet () {
         />
       ))}
     </div>
-    <div> <p>You Clicked: {count}</p>
-    <button onClick={increase} className='bg-green-700 text-white rounded' >Increase!</button>
-    <button onClick={decrease} className='bg-green-700 text-white rounded' >Decrease!</button>
-    <br />
-        <button onClick={reSet} className='bg-green-700 text-white rounded' >Reset!</button>
+    <div className='text-4xl space-x-30'> <h1> 😎</h1>  
+      <p>You Clicked: {count}</p>
+    <button onClick={increase} className='bg-green-700 text-white rounded p-4' >Increase!</button>
+     {count === 10 && (
+          <p style={{ color: "blue", fontWeight: "bold" }}>
+            🥳 You reached 10!
+          </p>
+        )}
+    <button onClick={decrease} className='bg-green-700 text-white rounded p-4' >Decrease!</button>
+        <button onClick={reSet} className='bg-green-700 text-white rounded p-4 ' >Reset!</button>
         </div>
+        <hr />
  </>    
   );
 }
